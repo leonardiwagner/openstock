88aafed1a0349e33d33a8cc4cd348b7cbace34fb
Wagner Leonardi <leonardiwagner@gmail.com>
2014-04-18 00:51:13 -0300

    creating some empty tests

---
+++ src/test/java/bankofjava/unit/MessageTest.java
@@ -0,0 +1,37 @@
+package bankofjava.unit;
+
+import org.junit.*;
+import bankofjava.domain.investment.*;
+
+public class MessageTest {
+	
+	@Test
+	public void createInvestment(){
+	
+	}
+	
+	@Test
+	public void upvoteInvestment(){
+		
+	}
+	
+	@Test
+	public void downvoteInvestment(){
+		
+	}
+	
+	@Test
+	public void receiveCoinFromUpvotedInvestment()
+	{
+		
+	}
+	
+	@Test
+	public void loseCoinFromUpvotedInvestment()
+	{
+		
+	}
+	
+	
+
+}
