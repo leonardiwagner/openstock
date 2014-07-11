5be20e0fd2b617216474fac0168642b399ebda16
Wagner Leonardi <leonardiwagner@gmail.com>
2014-07-11 15:31:34 -0300

    yahoo finance with currencies integration

--- src/test/java/bankofjava/integration/StockTest.java
+++ src/test/java/bankofjava/integration/StockTest.java
@@ -1,16 +1,34 @@
 package bankofjava.integration;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.Date;
+import java.util.List;
 
+import bankofjava.domain.StockItem;
 import org.junit.*;
 
 import bankofjava.infra.StockRepository;
 
 public class StockTest {
 	
-	@Test
-	public void createTestAccount(){
+	//@Test
+	public void testS(){
 		StockRepository stockRepository = new StockRepository();
 		stockRepository.GetHistoricalData("ople", new Date(), new Date());
 	}
+
+    @Test
+    public void createTestAccount(){
+        StockRepository stockRepository = new StockRepository();
+        List<StockItem> currencies = null;
+        try {
+            currencies = stockRepository.GetExchangeData();
+        }catch(URISyntaxException e){
+
+        }catch(IOException e){
+
+        }
+        Assert.assertEquals(currencies.size(), 5);
+    }
 }
