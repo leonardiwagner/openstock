18f66c58c5d5e89237045a04ccacaa7a84c280cc
wagner leonardi <leonardiwagner@gmail.com>
2014-07-17 22:43:53 -0300

    initializing html

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,40 @@
+
+// Definindo um novo módulo para nossa aplicação
+var homeApp = angular.module ("homeApp", []);
+
+
+homeApp.controller('HomeController', function($scope) {
+  $scope.moversUp = [
+    {
+      name: 'Coca Cola',
+      index: '3.5',
+    },
+    {
+      name: 'Microsoft',
+      index: '2.8',
+    },
+    {
+      name: 'Google',
+      index: '2.0',
+    }
+  ]; 
+
+  $scope.moversDown = [
+    {
+      name: 'Go Pro',
+      index: '-3.5',
+    },
+    {
+      name: 'Yahoo',
+      index: '-2.4',
+    },
+    {
+      name: 'Euro',
+      index: '-1.80',
+    }
+  ];
+});
+
+
+
+
